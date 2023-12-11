32e4055a7ed9cba73627aae55fd4d1e0a727ed3c
ShubhamGauniyal <84062824+ShubhamGauniyal@users.noreply.github.com>
2023-12-12 00:15:20 +0530

    Add files via upload
    New Update

---
+++ navigation.js
@@ -0,0 +1,14 @@
+$(document).ready(()=>{
+    $("#home").click(()=>{
+      window.open(`home.html` , "_self")
+    })
+    $("#partner").click(()=>{
+      window.open(`mackbook-99.html` , "_self")
+    })
+    $("#hedge").click(()=>{
+      window.open(`macbook-55.html` , "_self")
+    })
+    $("#solutions").click(()=>{
+      window.open(`macbook-77.html` , "_self")
+    })
+  })
